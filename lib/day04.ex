89ae7b2b6b4fecc514621c04c994fb673493a2e0
Mattias Lundberg <me@mlundberg.se>
2020-12-04 08:02:51 +0100

    Day 4 part 2

--- lib/day04.ex
+++ lib/day04.ex
@@ -10,21 +10,79 @@ defmodule Aoc2020.Day04 do
   ]
 
   def part1(input) do
-    required = MapSet.new(@required_keys)
-
     input
     |> Enum.map(&parse_passport_string/1)
-    |> Enum.map(&Map.keys/1)
-    |> Enum.map(&MapSet.new/1)
-    |> Enum.filter(&MapSet.subset?(required, &1))
+    |> Enum.map(&has_fields?/1)
+    |> Enum.filter(&Function.identity/1)
     |> length()
   end
 
-  def part2(_input) do
-    :ok
+  def part2(input) do
+    input
+    |> Enum.map(&parse_passport_string/1)
+    |> Enum.map(&validate_passport/1)
+    |> Enum.filter(&Function.identity/1)
+    |> length()
   end
 
-  defp parse_passport_string(str) do
+  def validate_passport(p) do
+    has_fields?(p) and byr_valid(p) and iyr_valid(p) and eyr_valid(p) and
+      hgt_valid(p) and hcl_valid(p) and ecl_valid(p) and pid_valid(p)
+  end
+
+  def byr_valid(%{"byr" => byr}) do
+    value = String.to_integer(byr)
+    value >= 1920 and value <= 2002
+  end
+
+  def iyr_valid(%{"iyr" => iyr}) do
+    value = String.to_integer(iyr)
+    value >= 2010 and value <= 2020
+  end
+
+  def eyr_valid(%{"eyr" => eyr}) do
+    value = String.to_integer(eyr)
+    value >= 2020 and value <= 2030
+  end
+
+  def hgt_valid(%{"hgt" => hgt}) do
+    case Integer.parse(hgt) do
+      {v, "cm"} ->
+        v >= 150 and v <= 193
+
+      {v, "in"} ->
+        v >= 59 and v <= 76
+
+      _ ->
+        false
+    end
+  end
+
+  def hcl_valid(%{"hcl" => "#" <> col}) do
+    Regex.match?(~r/^[0-9a-f]{6}$/, col)
+  end
+
+  def hcl_valid(_), do: false
+
+  def ecl_valid(%{"ecl" => ecl}) do
+    valid = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"]
+    ecl in valid
+  end
+
+  def pid_valid(%{"pid" => pid}) do
+    Regex.match?(~r/^[0-9]{9}$/, pid)
+  end
+
+  def has_fields?(p) do
+    required = MapSet.new(@required_keys)
+
+    p
+    |> Map.keys()
+    |> MapSet.new()
+    |> (&MapSet.subset?(required, &1)).()
+  end
+
+  def parse_passport_string(str) do
     str
     |> String.replace("\n", " ")
     |> String.split(" ")
--- test/aoc2020_day04_test.exs
+++ test/aoc2020_day04_test.exs
@@ -10,6 +10,14 @@ defmodule Aoc2020Day04Test do
                 |> String.trim()
                 |> String.split("\n\n")
 
+  @valid_data File.read!("inputs/day04_valid.txt")
+              |> String.trim()
+              |> String.split("\n\n")
+
+  @invalid_data File.read!("inputs/day04_invalid.txt")
+                |> String.trim()
+                |> String.split("\n\n")
+
   test "p1: default example" do
     assert Day04.part1(@example_data) == 2
   end
@@ -18,14 +26,62 @@ defmodule Aoc2020Day04Test do
     assert Day04.part1(@full_data) == 247
   end
 
-  @tag :skip
   test "p2: default examples" do
-    assert Day04.part2(@example_data) == 336
+    assert Day04.part2(@valid_data) == 4
+    assert Day04.part2(@invalid_data) == 0
   end
 
-  @tag :skip
+  test "byr_valid" do
+    refute Day04.byr_valid(%{"byr" => "1234"})
+    assert Day04.byr_valid(%{"byr" => "1920"})
+    assert Day04.byr_valid(%{"byr" => "2002"})
+    refute Day04.byr_valid(%{"byr" => "2003"})
+    refute Day04.byr_valid(%{"byr" => "2021"})
+  end
+
+  test "iyr_valid" do
+    refute Day04.iyr_valid(%{"iyr" => "1234"})
+    assert Day04.iyr_valid(%{"iyr" => "2010"})
+    assert Day04.iyr_valid(%{"iyr" => "2020"})
+  end
+
+  test "eyr_valid" do
+    refute Day04.eyr_valid(%{"eyr" => "2019"})
+    assert Day04.eyr_valid(%{"eyr" => "2030"})
+    assert Day04.eyr_valid(%{"eyr" => "2020"})
+  end
+
+  test "hgt_valid" do
+    assert Day04.hgt_valid(%{"hgt" => "60in"})
+    assert Day04.hgt_valid(%{"hgt" => "190cm"})
+    refute Day04.hgt_valid(%{"hgt" => "190in"})
+    refute Day04.hgt_valid(%{"hgt" => "60cm"})
+    refute Day04.hgt_valid(%{"hgt" => "60"})
+  end
+
+  test "hcl_valid" do
+    assert Day04.hcl_valid(%{"hcl" => "#123123"})
+    assert Day04.hcl_valid(%{"hcl" => "#ffffff"})
+    refute Day04.hcl_valid(%{"hcl" => "#fffff"})
+    refute Day04.hcl_valid(%{"hcl" => "#fffffff"})
+    refute Day04.hcl_valid(%{"hcl" => "fff"})
+  end
+
+  test "ecl_valid" do
+    assert Day04.ecl_valid(%{"ecl" => "amb"})
+    assert Day04.ecl_valid(%{"ecl" => "blu"})
+    assert Day04.ecl_valid(%{"ecl" => "gry"})
+    refute Day04.ecl_valid(%{"ecl" => "xyz"})
+  end
+
+  test "pid_valid" do
+    assert Day04.pid_valid(%{"pid" => "111111111"})
+    refute Day04.pid_valid(%{"pid" => "11111f111"})
+    refute Day04.pid_valid(%{"pid" => "11111111111"})
+    refute Day04.pid_valid(%{"pid" => "1111111"})
+  end
+
   test "p2: full" do
-    assert Day04.part2(@full_data) < 5_794_675_200
-    assert Day04.part2(@full_data) == 2_655_892_800
+    assert Day04.part2(@full_data) == 145
   end
 end
