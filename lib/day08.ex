70089915e5db6138b8fae795d852b7526b5d137b
Mattias Lundberg <me@mlundberg.se>
2020-12-08 09:20:43 +0100

    Cleanup

--- lib/day08.ex
+++ lib/day08.ex
@@ -1,15 +1,20 @@
 defmodule Aoc2020.Day08 do
   def part1(input) do
-    input |> Enum.map(&parse/1) |> run_until_loop(0, 0, [])
+    input |> Enum.map(&parse/1) |> run_program(0, 0, []) |> elem(1)
   end
 
   def part2(input) do
     instructions = input |> Enum.map(&parse/1)
 
     0..(length(instructions) - 1)
-    |> Enum.map(&check_terminates(&1, instructions))
-    |> Enum.filter(&Function.identity/1)
+    |> Enum.map(&update_instruction(&1, instructions))
+    |> Enum.map(&run_program(&1, 0, 0, []))
+    |> Enum.filter(fn
+      {:end, _} -> true
+      _ -> false
+    end)
     |> Enum.at(0)
+    |> elem(1)
   end
 
   defp parse(line) do
@@ -17,41 +22,24 @@ defmodule Aoc2020.Day08 do
     {op, String.to_integer(count)}
   end
 
-  defp check_terminates(loc_to_change, instructions) do
-    update_instruction(loc_to_change, instructions) |> run_until_terminate(0, 0, [])
-  end
-
   defp update_instruction(0, [{"nop", v} | rest]), do: [{"jmp", v} | rest]
   defp update_instruction(0, [{"jmp", v} | rest]), do: [{"nop", v} | rest]
   defp update_instruction(0, [{i, v} | rest]), do: [{i, v} | rest]
   defp update_instruction(loc, [head | rest]), do: [head | update_instruction(loc - 1, rest)]
 
-  defp run_until_terminate(instructions, loc, acc, visited) do
+  defp run_program(instructions, loc, acc, visited) do
     instruction = Enum.at(instructions, loc)
 
     if is_nil(instruction) do
-      acc
+      {:end, acc}
     else
-      [loc, acc] = run_instruction(instruction, loc, acc)
+      [new_loc, new_acc] = run_instruction(instruction, loc, acc)
 
-      if Enum.member?(visited, loc) do
-        false
+      if Enum.member?(visited, new_loc) do
+        {:loop, new_acc}
       else
-        run_until_terminate(instructions, loc, acc, [loc | visited])
+        run_program(instructions, new_loc, new_acc, [new_loc | visited])
       end
-    end
-  end
-
-  defp run_until_loop(instructions, loc, acc, visited) do
-    [loc, acc] =
-      instructions
-      |> Enum.at(loc)
-      |> run_instruction(loc, acc)
-
-    if Enum.member?(visited, loc) do
-      acc
-    else
-      run_until_loop(instructions, loc, acc, [loc | visited])
     end
   end
 
