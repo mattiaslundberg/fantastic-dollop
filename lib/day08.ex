708ec094f43afa44149a5e6bf49508c9787c6ecc
Mattias Lundberg <me@mlundberg.se>
2020-12-08 08:28:21 +0100

    Day 8 part 1

---
+++ lib/day08.ex
@@ -0,0 +1,31 @@
+defmodule Aoc2020.Day08 do
+  def part1(input) do
+    input |> Enum.map(&parse/1) |> run_program(0, 0, [])
+  end
+
+  def part2(_input) do
+    :ok
+  end
+
+  defp parse(line) do
+    [op, count] = String.split(line, " ")
+    {op, String.to_integer(count)}
+  end
+
+  defp run_program(instructions, loc, acc, visited) do
+    [loc, acc] =
+      instructions
+      |> Enum.at(loc)
+      |> run_instruction(loc, acc)
+
+    if Enum.member?(visited, loc) do
+      acc
+    else
+      run_program(instructions, loc, acc, [loc | visited])
+    end
+  end
+
+  defp run_instruction({"nop", _}, loc, acc), do: [loc + 1, acc]
+  defp run_instruction({"acc", v}, loc, acc), do: [loc + 1, acc + v]
+  defp run_instruction({"jmp", v}, loc, acc), do: [loc + v, acc]
+end
---
+++ test/aoc2020_day08_test.exs
@@ -0,0 +1,30 @@
+defmodule Aoc2020Day08Test do
+  use ExUnit.Case, async: true
+  alias Aoc2020.Day08
+
+  @full_data File.read!("inputs/day08.txt")
+             |> String.trim()
+             |> String.split("\n")
+
+  @example_data File.read!("inputs/day08_example.txt")
+                |> String.trim()
+                |> String.split("\n")
+
+  test "p1: default example" do
+    assert Day08.part1(@example_data) == 5
+  end
+
+  test "p1: full" do
+    assert Day08.part1(@full_data) == 1797
+  end
+
+  @tag :skip
+  test "p2: default example" do
+    assert Day08.part2(@example_data) == 32
+  end
+
+  @tag :skip
+  test "p2: full" do
+    assert Day08.part2(@full_data) == 108_636
+  end
+end
