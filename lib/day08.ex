32766530c7c7e8f8d6f5d9817aea668ca5dcbe99
Mattias Lundberg <me@mlundberg.se>
2020-12-08 09:00:32 +0100

    Day 8 part 2

--- lib/day08.ex
+++ lib/day08.ex
@@ -3,13 +3,43 @@ defmodule Aoc2020.Day08 do
     input |> Enum.map(&parse/1) |> run_until_loop(0, 0, [])
   end
 
-  def part2(_input) do
-    :ok
+  def part2(input) do
+    instructions = input |> Enum.map(&parse/1)
+
+    0..(length(instructions) - 1)
+    |> Enum.map(&check_terminates(&1, instructions))
+    |> Enum.filter(&Function.identity/1)
+    |> Enum.at(0)
   end
 
   defp parse(line) do
     [op, count] = String.split(line, " ")
     {op, String.to_integer(count)}
+  end
+
+  defp check_terminates(loc_to_change, instructions) do
+    update_instruction(loc_to_change, instructions) |> run_until_terminate(0, 0, [])
+  end
+
+  defp update_instruction(0, [{"nop", v} | rest]), do: [{"jmp", v} | rest]
+  defp update_instruction(0, [{"jmp", v} | rest]), do: [{"nop", v} | rest]
+  defp update_instruction(0, [{i, v} | rest]), do: [{i, v} | rest]
+  defp update_instruction(loc, [head | rest]), do: [head | update_instruction(loc - 1, rest)]
+
+  defp run_until_terminate(instructions, loc, acc, visited) do
+    instruction = Enum.at(instructions, loc)
+
+    if is_nil(instruction) do
+      acc
+    else
+      [loc, acc] = run_instruction(instruction, loc, acc)
+
+      if Enum.member?(visited, loc) do
+        false
+      else
+        run_until_terminate(instructions, loc, acc, [loc | visited])
+      end
+    end
   end
 
   defp run_until_loop(instructions, loc, acc, visited) do
--- test/aoc2020_day08_test.exs
+++ test/aoc2020_day08_test.exs
@@ -18,13 +18,11 @@ defmodule Aoc2020Day08Test do
     assert Day08.part1(@full_data) == 1797
   end
 
-  @tag :skip
   test "p2: default example" do
-    assert Day08.part2(@example_data) == 32
+    assert Day08.part2(@example_data) == 8
   end
 
-  @tag :skip
   test "p2: full" do
-    assert Day08.part2(@full_data) == 108_636
+    assert Day08.part2(@full_data) == 1036
   end
 end
