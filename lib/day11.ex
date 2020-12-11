133cf2fc7a6acde3917d95887081ddde86c117a6
Mattias Lundberg <me@mlundberg.se>
2020-12-11 14:19:08 +0100

    Day 11 part 2

--- lib/day11.ex
+++ lib/day11.ex
@@ -8,9 +8,24 @@ defmodule Aoc2020.Day11 do
     input
     |> into_map()
     |> do_move(&count_neighbors/4, 4, rows, cols)
+    |> count_occupied()
+  end
+
+  def part2(input) do
+    rows = length(input)
+    cols = input |> Enum.at(0) |> String.length()
+
+    input
+    |> into_map()
+    |> do_move(&count_directions/4, 5, rows, cols)
+    |> count_occupied()
+  end
+
+  defp count_occupied(map) do
+    map
     |> Map.values()
     |> Enum.reduce(0, fn s, acc ->
-      acc + (s |> String.graphemes() |> Enum.count(fn c -> c == "#" end))
+      acc + if s == :occupied, do: 1, else: 0
     end)
   end
 
@@ -18,29 +33,31 @@ defmodule Aoc2020.Day11 do
     chunk_size = input |> Enum.at(0) |> String.length()
 
     for c <- 0..(length(input) - 1), r <- 0..(chunk_size - 1) do
-      val = input |> Enum.at(c, "") |> String.at(r)
+      val =
+        case input |> Enum.at(c, "") |> String.at(r) do
+          "#" -> :occupied
+          "L" -> :free
+          "." -> :floor
+        end
+
       {{c, r}, val}
     end
     |> Map.new()
-  end
-
-  def part2(input) do
-    :ok
-    # input
-    # |> do_move(&count_directions/4, 5)
-    # |> Enum.reduce(0, fn s, acc ->
-    #   acc + (s |> String.graphemes() |> Enum.count(fn c -> c == "#" end))
-    # end)
   end
 
   def do_move(input, count_fn, max_neighs, cols, rows) do
     new_input =
       for c <- 0..(cols - 1), r <- 0..(rows - 1) do
         val =
-          case get_elem(input, c, r) do
-            "L" -> if count_fn.(input, c, r, 1) == 0, do: "#", else: "L"
-            "#" -> if count_fn.(input, c, r, max_neighs) >= max_neighs, do: "L", else: "#"
-            c -> c
+          case Map.get(input, {c, r}) do
+            :free ->
+              if count_fn.(input, c, r, 1) == 0, do: :occupied, else: :free
+
+            :occupied ->
+              if count_fn.(input, c, r, max_neighs) >= max_neighs, do: :free, else: :occupied
+
+            v ->
+              v
           end
 
         {{c, r}, val}
@@ -55,9 +72,8 @@ defmodule Aoc2020.Day11 do
   end
 
   def count_directions(input, c, r, max) do
-    @directions
-    |> Enum.reduce_while(0, fn [dc, dr], acc ->
-      new = acc + count_direction(input, c, r, dc, dr)
+    Enum.reduce_while(@directions, 0, fn [dc, dr], acc ->
+      new = acc + count_direction(input, c + dc, r + dr, dc, dr)
 
       if new >= max do
         {:halt, new}
@@ -67,24 +83,21 @@ defmodule Aoc2020.Day11 do
     end)
   end
 
-  def count_direction(_input, _c, _r, 0, 0), do: 0
-
   def count_direction(input, c, r, dc, dr) do
-    case get_elem(input, c, r) do
-      "#" -> 1
-      "L" -> 0
-      "." -> count_direction(input, c + dc, r + dr, dc, dr)
+    case Map.get(input, {c, r}) do
+      :occupied -> 1
+      :free -> 0
+      :floor -> count_direction(input, c + dc, r + dr, dc, dr)
       nil -> 0
     end
   end
 
   def count_neighbors(input, c, r, max) do
-    @directions
-    |> Enum.reduce_while(0, fn [dc, dr], acc ->
+    Enum.reduce_while(@directions, 0, fn [dc, dr], acc ->
       new =
         acc +
-          case get_elem(input, c + dc, r + dr) do
-            "#" -> 1
+          case Map.get(input, {c + dc, r + dr}) do
+            :occupied -> 1
             _ -> 0
           end
 
@@ -94,9 +107,5 @@ defmodule Aoc2020.Day11 do
         {:cont, new}
       end
     end)
-  end
-
-  defp get_elem(input, col, row) do
-    Map.get(input, {col, row})
   end
 end
--- test/aoc2020_day11_test.exs
+++ test/aoc2020_day11_test.exs
@@ -39,13 +39,14 @@ defmodule Aoc2020Day11Test do
     assert Day11.count_neighbors(input, 2, 3, 10) == 2
   end
 
-  @tag :skip
   test "count directions" do
-    input = [
-      "#.#",
-      ".##",
-      "#.#"
-    ]
+    input =
+      [
+        "#.#",
+        ".##",
+        "#.#"
+      ]
+      |> Day11.into_map()
 
     assert Day11.count_directions(input, 1, 1, 5) == 5
     assert Day11.count_directions(input, 1, 1, 3) == 3
@@ -63,13 +64,11 @@ defmodule Aoc2020Day11Test do
     assert Day11.part1(@full_data) == 2289
   end
 
-  @tag :skip
   test "p2: default example" do
     assert Day11.part2(@example_data) == 26
   end
 
-  @tag :skip
   test "p2: full" do
-    assert Day11.part2(@full_data) == :ok
+    assert Day11.part2(@full_data) == 2059
   end
 end
