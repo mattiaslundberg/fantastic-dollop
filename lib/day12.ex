c7fc56e034065498995ecb19b47cebf292b21670
Mattias Lundberg <me@mlundberg.se>
2020-12-12 10:24:58 +0100

    Day 12 part 2

--- lib/day12.ex
+++ lib/day12.ex
@@ -5,8 +5,9 @@ defmodule Aoc2020.Day12 do
     abs(a) + abs(b)
   end
 
-  def part2(_input) do
-    :ok
+  def part2(input) do
+    {a, b, _, _} = input |> Enum.map(&transform/1) |> Enum.reduce({0, 0, 1, 10}, &move2/2)
+    abs(a) + abs(b)
   end
 
   def transform("F" <> num), do: {:forward, String.to_integer(num)}
@@ -16,6 +17,18 @@ defmodule Aoc2020.Day12 do
   def transform("E" <> num), do: {:east, String.to_integer(num)}
   def transform("S" <> num), do: {:south, String.to_integer(num)}
   def transform("W" <> num), do: {:west, String.to_integer(num)}
+
+  def move2({:forward, amount}, {a, b, wa, wb}), do: {a + amount * wa, b + amount * wb, wa, wb}
+  def move2({:right, amount}, {a, b, wa, wb}), do: rotate2(amount, a, b, wa, wb)
+  def move2({:left, amount}, {a, b, wa, wb}), do: rotate2(360 - amount, a, b, wa, wb)
+  def move2({:north, amount}, {a, b, wa, wb}), do: {a, b, wa + amount, wb}
+  def move2({:south, amount}, {a, b, wa, wb}), do: {a, b, wa - amount, wb}
+  def move2({:east, amount}, {a, b, wa, wb}), do: {a, b, wa, wb + amount}
+  def move2({:west, amount}, {a, b, wa, wb}), do: {a, b, wa, wb - amount}
+
+  defp rotate2(90, a, b, wa, wb), do: {a, b, -wb, wa}
+  defp rotate2(180, a, b, wa, wb), do: {a, b, -wa, -wb}
+  defp rotate2(270, a, b, wa, wb), do: {a, b, wb, -wa}
 
   def move({:forward, amount}, {a, b, :east}), do: {a, b + amount, :east}
   def move({:forward, amount}, {a, b, :north}), do: {a + amount, b, :north}
--- test/aoc2020_day12_test.exs
+++ test/aoc2020_day12_test.exs
@@ -32,6 +32,32 @@ defmodule Aoc2020Day12Test do
     assert {_, _, :east} = Day12.move({:left, 270}, {0, 0, :north})
   end
 
+  test "2 move forward" do
+    assert {10, 100, 1, 10} = Day12.move2({:forward, 10}, {0, 0, 1, 10})
+    assert {38, 170, 4, 10} = Day12.move2({:forward, 7}, {10, 100, 4, 10})
+  end
+
+  test "2 move waypoint" do
+    assert {_, _, 11, 10} = Day12.move2({:north, 10}, {0, 0, 1, 10})
+    assert {_, _, 0, 10} = Day12.move2({:south, 4}, {10, 100, 4, 10})
+    assert {_, _, 4, 14} = Day12.move2({:east, 4}, {10, 100, 4, 10})
+    assert {_, _, 4, 6} = Day12.move2({:west, 4}, {10, 100, 4, 10})
+  end
+
+  test "2 rotate" do
+    assert {38, 170, 10, -4} = Day12.move2({:left, 90}, {38, 170, 4, 10})
+    assert {38, 170, -10, 4} = Day12.move2({:right, 90}, {38, 170, 4, 10})
+    assert {38, 170, -4, -10} = Day12.move2({:right, 180}, {38, 170, 4, 10})
+  end
+
+  test "2 example steps" do
+    assert {10, 100, 1, 10} = Day12.move2({:forward, 10}, {0, 0, 1, 10})
+    assert {10, 100, 4, 10} = Day12.move2({:north, 3}, {10, 100, 1, 10})
+    assert {38, 170, 4, 10} = Day12.move2({:forward, 7}, {10, 100, 4, 10})
+    assert {38, 170, -10, 4} = Day12.move2({:right, 90}, {38, 170, 4, 10})
+    assert {-72, 214, -10, 4} = Day12.move2({:forward, 11}, {38, 170, -10, 4})
+  end
+
   test "p1: default example" do
     assert Day12.part1(@example_data) == 25
   end
@@ -40,13 +66,14 @@ defmodule Aoc2020Day12Test do
     assert Day12.part1(@full_data) == 2847
   end
 
-  @tag :skip
   test "p2: default example" do
-    assert Day12.part2(@example_data) == :ok
+    assert Day12.part2(@example_data) == 286
   end
 
-  @tag :skip
   test "p2: full" do
-    assert Day12.part2(@full_data) == :ok
+    res = Day12.part2(@full_data)
+    assert res > 16307
+    assert res < 51849
+    assert res == 29839
   end
 end
