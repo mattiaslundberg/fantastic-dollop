8251e262568580ff09d3ac4ffe0512b5bd2c2b0b
Mattias Lundberg <me@mlundberg.se>
2020-12-15 13:20:24 +0100

    Cleanup

--- lib/day15.ex
+++ lib/day15.ex
@@ -1,9 +1,9 @@
 defmodule Aoc2020.Day15 do
-  def part1(numbers), do: find_spoken_at(numbers, 2021, 1, nil, %{})
+  def part1(numbers), do: find_spoken_at(numbers, 2020, 1, nil, %{})
 
-  def part2(numbers), do: find_spoken_at(numbers, 30_000_001, 1, nil, %{})
+  def part2(numbers), do: find_spoken_at(numbers, 30_000_000, 1, nil, %{})
 
-  def find_spoken_at(_, target, target, last, _), do: last
+  def find_spoken_at(_, target, turn, last, _) when target + 1 == turn, do: last
 
   def find_spoken_at([n | nr], target, turn, _last, cache) do
     cache = update_cache(cache, n, turn)
@@ -16,8 +16,8 @@ defmodule Aoc2020.Day15 do
     find_spoken_at([], target, turn + 1, new, cache)
   end
 
-  def number_to_say([v, vv | _]), do: v - vv
+  def number_to_say([v, vv]), do: v - vv
   def number_to_say(_), do: 0
 
-  def update_cache(cache, new, turn), do: Map.update(cache, new, [turn], fn v -> [turn | v] end)
+  def update_cache(cache, new, turn), do: Map.update(cache, new, [turn], &[turn, hd(&1)])
 end
