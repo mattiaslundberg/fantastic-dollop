9f77548b63fb6d99573113a66e0bd95306da23da
Mattias Lundberg <me@mlundberg.se>
2020-12-18 07:56:29 +0100

    Day 18 part 1

--- lib/day18.ex
+++ lib/day18.ex
@@ -1,6 +1,60 @@
 defmodule Aoc2020.Day18 do
-  def part1(_input) do
-    :ok
+  def part1(input) when is_binary(input), do: part1([input])
+
+  def part1(input) do
+    input
+    |> Enum.reduce(0, fn line, acc ->
+      acc + (line |> String.replace(" ", "") |> calc([]))
+    end)
+  end
+
+  def calc("", [current]), do: current
+
+  def calc("+" <> rest, [prev]), do: calc(rest, [&Kernel.+/2, prev])
+  def calc("-" <> rest, [prev]), do: calc(rest, [&Kernel.-/2, prev])
+  def calc("*" <> rest, [prev]), do: calc(rest, [&Kernel.*/2, prev])
+
+  def calc("(" <> rest, prev) do
+    [inner, remaining] = find_end(rest, 0)
+    inner_res = calc(inner, [])
+
+    case prev do
+      [op, prev_res] ->
+        calc(remaining, [op.(prev_res, inner_res)])
+
+      [] ->
+        calc(remaining, [inner_res])
+    end
+  end
+
+  def calc(<<c::utf8>> <> rest, current) do
+    new_val = String.to_integer(<<c::utf8>>)
+
+    case {current, new_val} do
+      {[op, v1], v2} ->
+        calc(rest, [op.(v1, v2)])
+
+      {[], v} ->
+        calc(rest, [v])
+    end
+  end
+
+  def find_end(")" <> r, 0), do: ["", r]
+
+  def find_end(<<char::utf8>> <> r, c) do
+    [inner, remaining] =
+      case <<char::utf8>> do
+        "(" ->
+          find_end(r, c + 1)
+
+        ")" ->
+          find_end(r, c - 1)
+
+        _ ->
+          find_end(r, c)
+      end
+
+    [<<char::utf8>> <> inner, remaining]
   end
 
   def part2(_input) do
--- test/aoc2020_day18_test.exs
+++ test/aoc2020_day18_test.exs
@@ -6,21 +6,22 @@ defmodule Aoc2020Day18Test do
              |> String.trim()
              |> String.split("\n")
 
-  @example_data []
-
-  @tag :skip
   test "p1: default example" do
-    assert Day18.part1(@example_data) == :ok
+    assert Day18.part1("1 + 2 * 3 + 4 * 5 + 6") == 71
+    assert Day18.part1("2 * 3 + (4 * 5)") == 26
+    assert Day18.part1("5 + (8 * 3 + 9 + 3 * 4 * 3)") == 437
+    assert Day18.part1("5 * 9 * (7 * 3 * 3 + 9 * 3 + (8 + 6 * 4))") == 12240
+    assert Day18.part1("((2 + 4 * 9) * (6 + 9 * 8 + 6) + 6) + 2 + 4 * 2") == 13632
+    assert Day18.part1(["1 + 2 * 3 + 4 * 5 + 6", "2 * 3 + (4 * 5)"]) == 71 + 26
   end
 
-  @tag :skip
   test "p1: full" do
-    assert Day18.part1(@full_data) == :ok
+    assert Day18.part1(@full_data) == 69_490_582_260
   end
 
   @tag :skip
   test "p2: default example" do
-    assert Day18.part2(@example_data) == :ok
+    # assert Day18.part2(@example_data) == :ok
   end
 
   @tag :skip
