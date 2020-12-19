815ba260258b0b5800199bb2aeead136e34219c4
Mattias Lundberg <me@mlundberg.se>
2020-12-19 09:29:56 +0100

    Day 19 part 1

--- lib/day19.ex
+++ lib/day19.ex
@@ -1,9 +1,83 @@
 defmodule Aoc2020.Day19 do
-  def part1(_input) do
-    :ok
+  def part1([rule_input, messages_input]) do
+    rules = parse_rules(rule_input)
+
+    messages_input
+    |> String.split("\n")
+    |> Enum.map(fn m ->
+      matches_rule(rules, m |> String.codepoints(), 0)
+    end)
+    |> Enum.reduce(0, fn
+      {true, []}, r -> r + 1
+      _, r -> r
+    end)
   end
 
   def part2(_input) do
     :ok
+  end
+
+  def matches_rule(_, [], _), do: {false, []}
+
+  def matches_rule(rules, message, rule_id) do
+    rule = Map.get(rules, rule_id)
+
+    case rule do
+      r when is_binary(r) ->
+        {r == hd(message), tl(message)}
+
+      [l1, l2] when is_list(l1) and is_list(l2) ->
+        check_branch(rules, message, l1, l2)
+
+      l when is_list(l) ->
+        check_list(rules, message, l)
+    end
+  end
+
+  defp check_branch(rules, message, l1, l2) do
+    {res1, rem1} = check_list(rules, message, l1)
+    {res2, rem2} = check_list(rules, message, l2)
+
+    case {res1, res2} do
+      {true, false} -> {true, rem1}
+      {false, true} -> {true, rem2}
+      {true, true} -> {true, :FIXME}
+      {false, false} -> {false, []}
+    end
+  end
+
+  defp check_list(rules, message, l) do
+    Enum.reduce(l, {true, message}, fn
+      _, {false, rem} -> {false, rem}
+      r, {true, rem} -> matches_rule(rules, rem, r)
+    end)
+  end
+
+  def parse_rules(rule_input) do
+    rule_input |> String.split("\n") |> Enum.reduce(Map.new(), &parse_rule_line/2)
+  end
+
+  def parse_rule_line(rule_line, rules) do
+    [index, rule] = rule_line |> String.split(": ")
+
+    Map.put(rules, String.to_integer(index), parse_rule(rule))
+  end
+
+  def parse_rule(rule) do
+    cond do
+      String.starts_with?(rule, "\"") ->
+        String.split(rule, "\"") |> Enum.at(1)
+
+      String.contains?(rule, "|") ->
+        rule
+        |> String.split("|")
+        |> Enum.map(&String.split(&1, " ", trim: true))
+        |> Enum.map(fn l -> Enum.map(l, &String.to_integer/1) end)
+
+      true ->
+        rule
+        |> String.split(" ", trim: true)
+        |> Enum.map(&String.to_integer/1)
+    end
   end
 end
--- test/aoc2020_day19_test.exs
+++ test/aoc2020_day19_test.exs
@@ -4,18 +4,67 @@ defmodule Aoc2020Day19Test do
 
   @full_data File.read!("inputs/day19.txt")
              |> String.trim()
-             |> String.split("\n")
+             |> String.split("\n\n")
 
-  @example_data []
+  @example_data File.read!("inputs/day19_example.txt")
+                |> String.trim()
+                |> String.split("\n\n")
 
-  @tag :skip
-  test "p1: default example" do
-    assert Day19.part1(@example_data) == :ok
+  test "parse rules" do
+    assert Day19.parse_rules(Enum.at(@example_data, 0)) == %{
+             0 => [4, 1, 5],
+             1 => [[2, 3], [3, 2]],
+             2 => [[4, 4], [5, 5]],
+             3 => [[4, 5], [5, 4]],
+             4 => "a",
+             5 => "b"
+           }
+
+    assert Day19.parse_rules(Enum.at(@full_data, 0)) != %{}
   end
 
-  @tag :skip
+  test "new" do
+    assert Day19.matches_rule(%{0 => "a"}, ["a"], 0) == {true, []}
+    assert Day19.matches_rule(%{0 => "a"}, ["a", "b"], 0) == {true, ["b"]}
+    assert Day19.matches_rule(%{0 => "a"}, [], 0) == {false, []}
+
+    assert Day19.matches_rule(%{0 => [1, 2], 1 => "a", 2 => "b"}, ["a", "b"], 0) == {true, []}
+    assert Day19.matches_rule(%{0 => [1, 2], 1 => "a", 2 => "b"}, ["a"], 0) == {false, []}
+
+    assert Day19.matches_rule(%{0 => [1, 2], 1 => "a", 2 => "b"}, ["a", "b", "a"], 0) ==
+             {true, ["a"]}
+
+    assert Day19.matches_rule(%{0 => [1, 3], 1 => "a", 2 => "b", 3 => [2]}, ["a", "b"], 0) ==
+             {true, []}
+  end
+
+  test "matches rule" do
+    assert Day19.matches_rule(
+             %{0 => [[1, 2], [2, 1]], 1 => "a", 2 => "b"},
+             ["a", "b"],
+             0
+           ) == {true, []}
+
+    assert Day19.matches_rule(
+             %{0 => [[1, 2], [2, 1]], 1 => "a", 2 => "b"},
+             ["a", "b", "a"],
+             0
+           ) == {true, ["a"]}
+
+    assert Day19.matches_rule(
+             %{0 => [[1, 2], [2, 1]], 1 => "a", 2 => "b"},
+             ["b", "a"],
+             0
+           ) == {true, []}
+  end
+
+  test "p1: default example" do
+    assert Day19.part1(@example_data) == 2
+  end
+
   test "p1: full" do
-    assert Day19.part1(@full_data) == :ok
+    res = Day19.part1(@full_data)
+    assert res == 142
   end
 
   @tag :skip
